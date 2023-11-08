d8b72d034639c4938475aeab7faab748b81ea75e
Arsen Azizkhanov <a.azizkhanov.it@gmail.com>
2023-11-08 12:13:14 +0300

    Done 09

--- 09-props/src/App.js
+++ 09-props/src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
+import PetInfo from './components/PetInfo'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <PetInfo animal='cat' age='3' /> {/* здесь age = строка */}
+      <PetInfo animal='dog' age={5} /> {/* здесь age = число */}
+    </div>
+  )
 }
 
 export default App
