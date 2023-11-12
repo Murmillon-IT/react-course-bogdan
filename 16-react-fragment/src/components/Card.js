80eb7fbc100eca84f03e3004068c93b024388a33
Arsen Azizkhanov <a.azizkhanov.it@gmail.com>
2023-11-12 11:08:58 +0300

    Done 16

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,12 @@
+const Card = () => {
+    /* React Fragment */
+  return (
+    <>
+      <h1>Bogdan</h1>
+      <h3>I am recording React course</h3>
+      <button>Like!</button>
+    </>
+  )
+}
+
+export default Card
