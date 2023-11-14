c1845ac5ecb819e52a320acc4a0a54a6e9602f1c
Arsen Azizkhanov <a.azizkhanov.it@gmail.com>
2023-11-14 12:08:53 +0300

    Done 23

--- 23-css-modules/src/App.js
+++ 23-css-modules/src/App.js
@@ -1,7 +1,17 @@
+import Info from './components/Info.js'
 import './App.css'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Info />
+      <div className='info'>
+        <h1>App component heading</h1>
+        <h2>Heading in the Info component</h2>
+        <button className='my-button'>App component button</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
